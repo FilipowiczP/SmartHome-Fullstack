2b2ded8b1b0d3f9aced4cae563783b91abd2ab44
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2021-02-22 18:55:04 +0100

    create repo

---
+++ public/src/components/homeHeader/homeHeader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const homeHeader = () => {
+  return <header></header>;
+};
+
+export default homeHeader;
---
+++ public/src/views/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "../../components/homeHeader/homeHeader";
+
+const Home = () => {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+};
+
+export default Home;
