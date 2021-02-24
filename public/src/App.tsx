622dfd4659c42ca491458c8d160da591771deb7a
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2021-02-24 22:51:53 +0100

    Created theme

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme/theme";
+import { Body } from "./theme/global";
 
 import Home from "./views/Home/Home";
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <Body />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
---
+++ public/src/theme/theme.ts
@@ -0,0 +1,7 @@
+export const lightTheme = {
+  background: "linear-gradient(rgb(255, 138, 60), rgb(255, 160, 90))",
+};
+
+export const darkTheme = {
+  background: "linear-gradient(rgb(0, 0, 0), rgb(255, 255, 255))",
+};
