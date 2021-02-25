9c09b858b6b2315c2e92e1036b355cebecdc1024
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2021-02-25 13:30:09 +0100

    created header

--- public/src/components/homeHeader/homeHeader.tsx
+++ public/src/components/homeHeader/homeHeader.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import "./homeHeader";
+import plus from "../../images/plus.png";
+import "./homeHeader.scss";
 
 const homeHeader = () => {
-  return <header className="homeHeader"></header>;
+  return (
+    <header className="homeHeader">
+      <h1 className="homeHeader__title">Welcome</h1>
+      <div className="homeHeader__newRoom">
+        <img src={plus} alt="plus" />
+      </div>
+    </header>
+  );
 };
 
 export default homeHeader;
